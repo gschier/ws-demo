6cc04a2a50e9e6da6a2f65129b7ff642a927424f
Gregory Schier <gschier1990@gmail.com>
2021-12-15 15:23:56 -0800

    Exact dependencies

--- client/index.js
+++ client/index.js
@@ -4,23 +4,6 @@ const client = createClient({
   url: 'ws://localhost:4000/graphql',
 });
 
-// query
-(async () => {
-  const result = await new Promise((resolve, reject) => {
-    let result;
-    client.subscribe(
-      {
-        query: '{ hello }',
-      },
-      {
-        next: (data) => (result = data),
-        error: reject,
-        complete: () => resolve(result),
-      },
-    );
-  });
-})();
-
 // subscription
 (async () => {
   const onNext = () => {
